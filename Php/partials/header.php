62ccf54e7fab8b5e1fee49ab4c931692e56a6955
Vane <vanne-894@outlook.es>
2020-01-21 01:15:58 -0300

    arreglando php estructurado

--- Php/partials/header.php
+++ Php/partials/header.php
@@ -6,7 +6,7 @@
           <div class="d-flex flex-grow-1">
               <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
               <div class="navbar-brand" href="#">
-                  <a href="index.php"><img class="__imglogo" src="https://www.digitalhouse.com/logo-DH.png" alt="logo de Digital House" width="300"></a> 
+                  <a href="index.php"><img class="__imglogo" src="https://www.digitalhouse.com/logo-DH.png" alt="logo de Digital House" width="300"></a>
               </div>
               <div class="w-100 text-right">
                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar7">
@@ -24,7 +24,7 @@
                       <a href="#" class="nav-link">Publicidad  Eventos</a>
                   </li>
                   <!--Esto lo estoy colocando para controlar que debe aparecer en las opciones del navbar, dependiendo si el usuario está logueado o no-->
-                  
+
                   <?php if(!isset($_SESSION["email"])) :?>
                         <li class="nav-item o_navitem">
                             <a href="login.php" class="nav-link">Logín</a>
@@ -33,19 +33,23 @@
                             <a href="register.php" class="nav-link">Registro</a>
                         </li>
                   <?php else :?>
-                    
+
                         <li class="nav-item o_navitem">
                         <a href="logout.php" class="nav-link">Cerrar Sesión </a>
                       </li>
 
-                  <?php endif;?>    
+                  <?php endif;?>
               </ul>
 
           </div>
-          <div class="d-flex __busqueda">
+          <?php if(isset($_SESSION["email"])) :?>
+
+           <div class="d-flex __busqueda">
             <form class="d-flex form-inline">
               <input class="form-control mr-sm-2" type="search" placeholder="Busca tu pelicula" aria-label="Search">
               <button class="btn btn-dark my-2 my-sm-0 __buscar" type="submit">Buscar</button>
             </form>
             </div>
+          <?php endif;?>
+
         </nav>
