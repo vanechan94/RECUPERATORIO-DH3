c1ecf755818bc00ab6646c56b986fa2d28b36a74
Vane <vanne-894@outlook.es>
2020-01-21 01:45:02 -0300

    problemas con persistencia

--- Php/register.php
+++ Php/register.php
@@ -13,10 +13,15 @@
       $registro = armarRegistro($_POST,$avatar);
       guardarRegistro($registro);
      //De no excistir errores en la información tipeada por el usuario entonces lo redirecciono a donde yo desee.
-
       header("location:index.php");
       exit;
-    }     
+    }
+  }else{
+    //PERSISTENCIA
+    $userName = $_POST['userName'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $passwordRepeat = $_POST['passwordRepeat'];
 
   }
 }
@@ -32,63 +37,63 @@
   </head>
   <body>
     <div class="container-fluid">
-      <?php include_once('partials/header.php');?>  
+      <?php include_once('partials/header.php');?>
       <section class="formulario">
         <div id="formContainer" class="row align-items-center">
           <div class="col-8 offset-2  ">
             <h1>Registración</h1>
-            <?php if(isset($errores)):?>
-              <ul class="alert alert-danger">
-                <?php foreach ($errores as $value) :?>
-                    <li><?=$value;?></li>
-                <?php endforeach;?>
-              </ul>
-            <?php endif;?> 
-
+<?php
+if(isset($errores)):?>
+<ul class="alert alert-danger">
+<?php foreach ($errores as $value) :?>
+<li><?=$value;?><li>
+<?php endforeach; ?>
+</ul>
+<?php endif; ?>
             <form id="formulario"  class="form" name="formRegistro" novalidate action="" method="POST" enctype="multipart/form-data">
               <div class="form-group">
                   <label for="userName">Nombre de usuario</label>
-                  <input required name="userName" type="text" value="" class="form-control" id="userName" placeholder="Nombre de usuario">
+                  <input required name="userName" type="text" value='<?= $userName ?? '' ?>' class="form-control" id="userName" placeholder="Nombre de usuario">
               </div>
               <div>
                 <!--Aquí les comparto este otro código donde pueden mostrar el error en el mismo lugar donde se produjo, usted decide como desea mostrar los errores-->
-                <?php if(isset($errores['userName'])):?>
-                  <h6 class="text-danger"><?= $errores['userName'];?></h6>  
-                <?php endif;?>
+               <?php if(isset($errores['userName'])):?>
+                 <h6 class="text-danger"><?= $errores['userName']; ?></h6>
+               <?php endif; ?>
               </div>
-                      
+
               <div class="form-group">
                 <label for="email">Correo electrónico</label>
-                <input required name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ingrese su correo" value="">
+                <input name="email" type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Ingrese su correo" value='<?= $email ?? '' ?>' required >
               </div>
-          
+
               <div class="form-group">
                 <label for="password">Contraseña</label>
-                <input required name="password" type="password" value= "" class="form-control" id="password" placeholder="Contraseña">
-              
+                <input required name="password" type="password" value= '<?= $password ?? '' ?>' class="form-control" id="password" placeholder="Contraseña">
+
                 <small class="form-text text-muted">Al menos 6 caracteres, debe contenter sólo números</small>
               </div>
-            
+
               <div class="form-group">
                 <label for="password">Repetir contraseña</label>
-                <input required name="passwordRepeat" type="password" value= ""class="form-control" id="passwordRepeat" placeholder="Repetir contraseña">
+                <input required name="passwordRepeat" type="password" value= '<?= $passwordRepeat ?? '' ?>' class="form-control" id="passwordRepeat" placeholder="Repetir contraseña">
               </div>
               <div class="form-group">
                 <label for="avatar">Avatar</label>
                 <input required name="avatar" type="file" value= "" class="form-control" id="avatar" >
-              </div>          
-           
+              </div>
+
               <button type="submit" class="btn btn-primary">Registrarme</button>
               <a href="login.php" class="btn btn-link">Ya poseo una cuenta</a>
             </form>
-        </div> 
+        </div>
       </section>
-      <?php include_once('partials/footer.php');?>  
+      <?php include_once('partials/footer.php');?>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    
+
   </body>
 </html>
